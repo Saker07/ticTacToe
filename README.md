9bdbd71bbc0029eed509a8f1735dd639d1d776b3
saker <gjoni.klaudio@gmail.com>
2022-08-07 23:24:45 +0100

    Finish game functionality and layouts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ticTacToe
 to do:
 -gui (both html and css)
--call display on reset
 -call some sort of animation and message and slight pause for draw/win before resetting the board
+    -done but the last mark doesn't completely show up before the alert is called.
 -ai
--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 function playerFactory(choice){
     let wins, sign;
-    wins= 0;
-    sign= choice;
+    let winDisplay;
+    wins = 0;
+    sign = choice;
+    winDisplay = document.querySelector(`.${sign} .winCount`);
+    console.log(winDisplay)
     const getSign = ()=>{return sign};
-    const addWin = ()=>{return ++wins};
+    const addWin = ()=>{
+        wins++;
+        winDisplay.textContent = wins;
+        return wins;
+    };
     return {getSign, addWin}
 }
 function gameboardFactory(){
     let board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
     const reset = ()=>{
         board = [["", "", ""], ["", "", ""], ["", "", ""]];
+        display();
+        return;
     };
     const check = ()=>{
         let f=2;                    //default state "draw"
@@ -24,27 +33,35 @@ function gameboardFactory(){
         })();
         (function checkWin() {
             for (let i=0; i<board.length; i++){    //if someone won set to 1
-                if (board[i][0] == board[i][1] == board[i][2]){
+                if ([board[i][0], board[i][1], board[i][2]].every(item => item === currentPlayer.getSign())){
                     f=1;
                 }
-                if (board[0][i] == board[1][i] == board[2][i]){
+                if ([board[0][i], board[1][i], board[2][i]].every(item => item === currentPlayer.getSign())){
+                    f=1;
+
+                }
+                if ([board[0][2], board[1][1], board[2][0]].every(item => item === currentPlayer.getSign()) 
+                || [board[0][0], board[1][1], board[2][2]].every(item => item === currentPlayer.getSign())){
                     f=1;
                 }
-                if ((board[0][0] == board[1][1] == board[2][2]) || (board[0][2] == board[1][1] == board[2][0])){
-                    f=1;
-                }
+
             };
         })();
         if(f==1){   //win
             currentPlayer.addWin();
+            alert(`Win!`);
             gameboard.reset();
         }else if(f==2){     //draw
             gameboard.reset();
         }
         return f;
     };
-    const mark = (player, x, y)=>{
-        board[x][y] = board[x][y] == "" ? player.getSign() : board[x][y];
+    const mark = (x, y)=>{
+        board[x][y] = board[x][y] == "" ? currentPlayer.getSign() : board[x][y];
+        display();
+        check();
+
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
     };
     const display = ()=>{
         let oldCont = document.querySelector(".boardContainer");
@@ -59,13 +76,30 @@ function gameboardFactory(){
             }
         }
         oldCont.innerHTML = newCont.innerHTML;
+        let cells;
+        cells=document.querySelectorAll(".cell");
+        for(let row in board){
+            for(let cell in board){
+                let currIndex = (+row * 3) + +cell;
+                cells[currIndex].addEventListener("click", e=>{mark(+row, +cell)})
+            }
+        }
     };
+    const getBoard = ()=>{  //for testing purposes.
+        return board;
+   }
     return {reset, check, mark, display}
 }
 
 
 let playerOne, playerTwo, currentPlayer;
-playerOne = playerFactory("x");
-playerTwo = playerFactory("o");
-let gameboard = gameboardFactory();
+playerTwo = playerFactory("O");
+playerOne = playerFactory("X");
+currentPlayer = playerOne;
+
+let gameboard;
+gameboard = gameboardFactory();
+
+
+
 console.log("1");
