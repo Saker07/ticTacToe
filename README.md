94dbbbaeaffb4b5ed55d994a2264944d4c2e7b4c
Saker07 <gjoni.klaudio@gmail.com>
2024-02-08 20:57:37 +0000

    Apply prettier

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ticTacToe
+
 to do:
 -gui (both html and css)
 -call some sort of animation and message and slight pause for draw/win before resetting the board
-    -done but the last mark doesn't completely show up before the alert is called.
+-done but the last mark doesn't completely show up before the alert is called.
 -ai
--- script.js
+++ script.js
@@ -1,98 +1,126 @@
-function playerFactory(choice){
-    let wins, sign;
-    let winDisplay;
-    wins = 0;
-    sign = choice;
-    winDisplay = document.querySelector(`.${sign} .winCount`);
-    console.log(winDisplay)
-    const getSign = ()=>{return sign};
-    const addWin = ()=>{
-        wins++;
-        winDisplay.textContent = wins;
-        return wins;
-    };
-    return {getSign, addWin}
+function playerFactory(choice) {
+  let wins, sign;
+  let winDisplay;
+  wins = 0;
+  sign = choice;
+  winDisplay = document.querySelector(`.${sign} .winCount`);
+  console.log(winDisplay);
+  const getSign = () => {
+    return sign;
+  };
+  const addWin = () => {
+    wins++;
+    winDisplay.textContent = wins;
+    return wins;
+  };
+  return { getSign, addWin };
 }
-function gameboardFactory(){
-    let board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
-    const reset = ()=>{
-        board = [["", "", ""], ["", "", ""], ["", "", ""]];
-        display();
-        return;
-    };
-    const check = ()=>{
-        let f=2;                    //default state "draw"
-        (function checkDraw() {      //if even 1 cell is empty, it's no longer a draw, so set to 0
-            for (let row of board){
-                for(let cell of row){
-                    if (cell === ""){
-                        f=0;
-                    }
-                }
-            }
-        })();
-        (function checkWin() {
-            for (let i=0; i<board.length; i++){    //if someone won set to 1
-                if ([board[i][0], board[i][1], board[i][2]].every(item => item === currentPlayer.getSign())){
-                    f=1;
-                }
-                if ([board[0][i], board[1][i], board[2][i]].every(item => item === currentPlayer.getSign())){
-                    f=1;
-
-                }
-                if ([board[0][2], board[1][1], board[2][0]].every(item => item === currentPlayer.getSign()) 
-                || [board[0][0], board[1][1], board[2][2]].every(item => item === currentPlayer.getSign())){
-                    f=1;
-                }
-
-            };
-        })();
-        if(f==1){   //win
-            currentPlayer.addWin();
-            alert(`Win!`);
-            gameboard.reset();
-        }else if(f==2){     //draw
-            alert("Draw!");
-            gameboard.reset();
+function gameboardFactory() {
+  let board = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+  ];
+  const reset = () => {
+    board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+    display();
+    return;
+  };
+  const check = () => {
+    let f = 2; //default state "draw"
+    (function checkDraw() {
+      //if even 1 cell is empty, it's no longer a draw, so set to 0
+      for (let row of board) {
+        for (let cell of row) {
+          if (cell === "") {
+            f = 0;
+          }
         }
-        return f;
-    };
-    const mark = (x, y)=>{
-        if(board[x][y] == ""){
-            board[x][y] = currentPlayer.getSign();
-            display();
-            check();
-            currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+      }
+    })();
+    (function checkWin() {
+      for (let i = 0; i < board.length; i++) {
+        //if someone won set to 1
+        if (
+          [board[i][0], board[i][1], board[i][2]].every(
+            (item) => item === currentPlayer.getSign()
+          )
+        ) {
+          f = 1;
         }
-    };
-    const display = ()=>{
-        let oldCont = document.querySelector(".boardContainer");
-        let newCont = document.createElement("div");
-        let elem;
-        for (let row of board){
-            for(let cell of row){
-                elem = document.createElement("div");
-                elem.classList.add("cell");
-                elem.textContent = cell;
-                newCont.appendChild(elem);
-            }
+        if (
+          [board[0][i], board[1][i], board[2][i]].every(
+            (item) => item === currentPlayer.getSign()
+          )
+        ) {
+          f = 1;
         }
-        oldCont.innerHTML = newCont.innerHTML;
-        let cells;
-        cells=document.querySelectorAll(".cell");
-        for(let row in board){
-            for(let cell in board){
-                let currIndex = (+row * 3) + +cell;
-                cells[currIndex].addEventListener("click", e=>{mark(+row, +cell)})
-            }
+        if (
+          [board[0][2], board[1][1], board[2][0]].every(
+            (item) => item === currentPlayer.getSign()
+          ) ||
+          [board[0][0], board[1][1], board[2][2]].every(
+            (item) => item === currentPlayer.getSign()
+          )
+        ) {
+          f = 1;
         }
-    };
-    const getBoard = ()=>{  //for testing purposes.
-        return board;
-   }
-    return {reset, check, mark, display}
+      }
+    })();
+    if (f == 1) {
+      //win
+      currentPlayer.addWin();
+      alert(`Win!`);
+      gameboard.reset();
+    } else if (f == 2) {
+      //draw
+      alert("Draw!");
+      gameboard.reset();
+    }
+    return f;
+  };
+  const mark = (x, y) => {
+    if (board[x][y] == "") {
+      board[x][y] = currentPlayer.getSign();
+      display();
+      check();
+      currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    }
+  };
+  const display = () => {
+    let oldCont = document.querySelector(".boardContainer");
+    let newCont = document.createElement("div");
+    let elem;
+    for (let row of board) {
+      for (let cell of row) {
+        elem = document.createElement("div");
+        elem.classList.add("cell");
+        elem.textContent = cell;
+        newCont.appendChild(elem);
+      }
+    }
+    oldCont.innerHTML = newCont.innerHTML;
+    let cells;
+    cells = document.querySelectorAll(".cell");
+    for (let row in board) {
+      for (let cell in board) {
+        let currIndex = +row * 3 + +cell;
+        cells[currIndex].addEventListener("click", (e) => {
+          mark(+row, +cell);
+        });
+      }
+    }
+  };
+  const getBoard = () => {
+    //for testing purposes.
+    return board;
+  };
+  return { reset, check, mark, display };
 }
-
 
 let playerOne, playerTwo, currentPlayer;
 playerTwo = playerFactory("O");
@@ -102,7 +130,5 @@ currentPlayer = playerOne;
 let gameboard;
 gameboard = gameboardFactory();
 gameboard.reset();
-
-
 
 console.log("1");
